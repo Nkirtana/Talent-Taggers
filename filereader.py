3cb8c5b4d95475b1dcc8ce4e21f6f0237e7df353
Manoj Buddha <manojnaidu001@gmail.com>
2023-02-28 11:45:43 -0500

    minor changes

--- filereader.py
+++ filereader.py
@@ -12,11 +12,11 @@ import os
 # It can detect extensions and read resumes based on extension.
 class ReadFiles:
 
-    def __init__(self, folder_path):
+    def __init__(self, input_path):
         self.files_path = list()
         self.text_dict = dict()
-        if os.path.exists(folder_path):
-            self.folder_path = folder_path
+        if os.path.exists(input_path):
+            self.input_path = input_path
         else:
             raise ValueError("Path not found!!")
 
@@ -28,32 +28,53 @@ class ReadFiles:
     # Get file extension
     @staticmethod
     def get_file_extension(filepath):
-        return os.path.splitext(filepath)[-1]
+        return os.path.splitext(filepath)[-1].lower()
+
+    def read_pdf(self, pdf_path):
+        reader = PdfReader(pdf_path)
+        text = ""
+        for page in reader.pages:
+            text = text + page.extract_text() + "\n"
+        self.text_dict[pdf_path] = text
+
+    def read_doc(self, doc_path):
+        text = docx2txt.process(doc_path)
+        self.text_dict[doc_path] = text
 
     # Read all the resumes from a given folder and return list of text from resumes
     def read_resumes(self):
-        self.files_path = list()
-        self.files_path = [os.path.join(self.folder_path, file) for file in self.get_files(self.folder_path)]
-        for path in self.files_path[:10]:
-            # Reading pdf
-            if self.get_file_extension(path) == '.pdf':
-                try:
-                    reader = PdfReader(path)
-                    text = ""
-                    for page in reader.pages:
-                        text = text + page.extract_text() + "\n"
-                    self.text_dict[path] = text
-                except:
-                    print(f"Returned error when reading pdf: {path}")
-            # Reading documents
-            elif self.get_file_extension(path) == '.docx':
-                try:
-                    self.text_dict[path] = docx2txt.process(path)
-                except:
-                    print(f"Returned error when reading document: {path}")
+
+        # Check if input path is file
+        if os.path.isfile(self.input_path):
+            if self.get_file_extension(self.input_path) == '.pdf':
+                self.read_pdf(self.input_path)
+
+            elif self.get_file_extension(self.input_path) == '.docx':
+                self.read_doc(self.input_path)
 
             else:
-                print(f"File format {self.get_file_extension(path)} not supported")
+                print(f"File format {self.get_file_extension(self.input_path)} not supported")
+
+        # Check if input path is directory
+        elif os.path.isdir(self.input_path):
+            self.files_path = list()
+            self.files_path = [os.path.join(self.input_path, file) for file in self.get_files(self.input_path)]
+            for path in self.files_path[:10]:
+                # Reading pdf
+                if self.get_file_extension(path) == '.pdf':
+                    try:
+                        self.read_pdf(path)
+                    except:
+                        print(f"Returned error when reading pdf: {path}")
+                # Reading documents
+                elif self.get_file_extension(path) == '.docx':
+                    try:
+                        self.read_doc(path)
+                    except:
+                        print(f"Returned error when reading document: {path}")
+
+                else:
+                    print(f"File format {self.get_file_extension(path)} not supported")
 
     def refresh_dict(self):
         self.text_dict = dict()
--- flaskapi.py
+++ flaskapi.py
@@ -4,31 +4,51 @@ from werkzeug.utils import secure_filename
 
 import os
 
+# https://flask.palletsprojects.com/en/2.2.x/patterns/fileuploads/
+# https://flask-restful.readthedocs.io/en/latest/quickstart.html#a-minimal-api
 
 app = Flask(__name__)
 api = Api(app)
 
-ALLOWED_EXTENSIONS = {'docx', 'pdf'}
+# file extensions API supports
+ALLOWED_EXTENSIONS = {'.docx', '.pdf'}
 basedir = os.path.abspath(os.path.dirname(__file__))
 UPLOAD_FOLDER = os.path.join(basedir, 'static')
 
+# Path where the files sent through api needs to be saved
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+# API class
 class SkillExtract(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-    def post(self):
+    @staticmethod
+    def get():
+        return {'message': 'API is up and running!'}
+
+    @staticmethod
+    def post():
         if request.method == 'POST':
-            print('POST request received')
-            print(request.files)
+            # print('POST request received')
+            # print(request.files)
+            if 'resume' not in request.files:
+                return {'message': 'file in not present in request'}
             file = request.files['resume']
-            print(file)
+            if file.filename == '':
+                return {'message': 'file in not selected'}
             filename = secure_filename(file.filename)
-            print(filename)
-            print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            if os.path.splitext(filepath)[-1].lower() in ALLOWED_EXTENSIONS:
+                file.save(filepath)
 
+            else:
+                return {'message': f"File format {os.path.splitext(filepath)[-1]} not supported!"}
+
+            return {'message': 'File loaded successfully!'}
+            # os.remove(filepath)
+
+
+# Routing for API
 api.add_resource(SkillExtract, '/')
 
 if __name__ == '__main__':
