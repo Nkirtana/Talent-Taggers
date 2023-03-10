1d44a716d2cc2fbdca2c61ec17a936233515ee2d
Manoj Buddha <manojnaidu001@gmail.com>
2023-03-10 18:00:10 -0500

    Added JWT authentication for API

--- flaskapi.py
+++ flaskapi.py
@@ -7,7 +7,6 @@ from skillextractor import ExtractSkills
 from filereader import ReadFiles
 
 from flask_jwt_extended import create_access_token
-from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
 
