226a8b24d5f767dd91fee4730def42d3933c6de3
Manoj Buddha <manojnaidu001@gmail.com>
2023-03-22 15:45:06 -0400

    added data cleaning part

--- datacleaning.py
+++ datacleaning.py
@@ -14,7 +14,6 @@ stopwords = stopwords.words('english')
 class CleanData:
     def __init__(self, stop_words=stopwords, punctuations=punctuation,
                  keep_punctuation=False, keep_stopwords=False):
-        self.cleaned_text = None
         self.input_text = None
         self.input_text_temp = None
         self.keep_punctuation = keep_punctuation
@@ -31,13 +30,13 @@ class CleanData:
     # Remove non-alphanumeric characters, punctuation flag to keep or remove punctuation
     def remove_non_alphanumeric(self):
         if self.keep_punctuation:
-            self.input_text = re.sub(f"[^a-zA-Z0-9_{self.punctuations}]", "", self.input_text)
+            self.input_text = re.sub(f"[^a-zA-Z0-9_{self.punctuations}]", " ", self.input_text)
         else:
-            self.input_text = re.sub(f"[^a-zA-Z0-9_]", "", self.input_text)
+            self.input_text = re.sub(f"[^a-zA-Z0-9_]", " ", self.input_text)
         print(f"Removed Non-alphanumeric characters, keep_punctuation:{self.keep_punctuation}")
 
     def remove_whitespace(self):
-        self.input_text = re.sub("[^ \t\n\r\f\v]", " ", self.input_text)
+        self.input_text = re.sub("[\t\n\r\f\v]", " ", self.input_text)
         print("removed white spaces")
 
     def remove_stopwords(self):
@@ -57,7 +56,14 @@ class CleanData:
     def generate_ngram(self, n):
         n_grams = ngrams(word_tokenize(self.input_text), n)
         n_grams = [' '.join(grams) for grams in n_grams]
-        self.output["ngrams"] = n_grams
+        if n == 1:
+            self.output["unigrams"] = n_grams
+        elif n == 2:
+            self.output["bigrams"] = n_grams
+        elif n == 3:
+            self.output["trigrams"] = n_grams
+        else:
+            self.output['ngram'] = n_grams
 
     def clean_text(self):
         self.remove_non_alphanumeric()
@@ -66,11 +72,26 @@ class CleanData:
         self.remove_extra_spaces()
         self.output["output_text"] = self.input_text
         self.output["input_text"] = self.input_text_temp
+        self.generate_ngram(2)
 
     def get_output(self):
         return self.output
 
-    def __repr__(self):
-        print(f"Keep punctuation flag: {self.keep_punctuation}")
-        print(f"Keep stopwords flag: {self.keep_stopwords}")
-        print(f"Custom punctuations: {self.punctuations}")
+    def __str__(self):
+        return f"Keep punctuation flag: {self.keep_punctuation}\nKeep stopwords flag: {self.keep_stopwords}" \
+               f"\nCustom punctuations: {self.punctuations}"
+
+
+# Testing
+from filereader import ReadFiles
+# rf = ReadFiles(r"C:\Users\manoj\Downloads\INFORMATION-TECHNOLOGY\10089434.pdf")
+# rf.read_input()
+# input_text = list(rf.text_dict.values())[0]
+# cd = CleanData(keep_stopwords=False, keep_punctuation=False)
+# cd.initialize_text(input_text)
+# cd.clean_text()
+# f = open("test.json", "w")
+# import json
+# json.dump(cd.output, f, indent = 4)
+# f.close()
+# print(cd)
